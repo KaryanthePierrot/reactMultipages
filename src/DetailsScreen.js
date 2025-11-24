a90ed7ec232065cf41363ebc35d0ed83e6ed03c9
Wes (Karyan) <113315411+KaryanthePierrot@users.noreply.github.com>
2025-11-24 11:16:10 -0300

    Enhance DetailsScreen with developer information
    Added detailed descriptions about the app and its developer.

--- src/DetailsScreen.js
+++ src/DetailsScreen.js
@@ -10,6 +10,36 @@ export default function DetailsScreen({ navigation }) {
         style={styles.button}>
         <Text style={styles.buttonText}>Ir Para Home</Text>
       </TouchableOpacity>
+
+      <View style={styles.desc}>
+       
+       <View>
+        <Text style={styles.text}>
+          Esta aplicação foi desenvolvida pelo aluno Westes Mesquita de DSM 4°;
+          na Fatec Itaquera - Miguel Reale.
+        </Text>
+        // <Text></Text>
+       </View>
+
+        <View>
+          <Text style={styles.text}>
+            O Dev em questão é programador mobile atualmente, especializado em
+            PHP e JAVA, porém como entusiasta de em desenvolvimento, está
+            experimentando com neste projeto sua recepção em React Native.
+          </Text>
+          // <Text></Text>
+        </View>
+
+        <View>
+          <Text style={styles.text}>
+            O App atualmente se chama MultiPages, e consta com 3 aplicações
+            distintas desenvolvidas previamente pelo autor ao longo do semestre
+            de 2035, e unificadas num só, contando também com uma página de
+            login, e registro.
+            <Text></Text>
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -22,7 +52,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#b1eaf1ff',
   },
   text: {
+    marginTop: 5,
+    marginBottom: 5, 
     fontSize: 24,
+    textAlign: 'center',
+  },
+  desc: {
+    width: '90%',
+    marginTop: 10
   },
   button: {
     backgroundColor: '#e9624bff',
